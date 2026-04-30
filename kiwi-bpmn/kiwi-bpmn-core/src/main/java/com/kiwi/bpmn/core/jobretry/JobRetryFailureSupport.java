98cb7ba1e389b4313e8970652b8ec2da64b4e835
x404 <xc350@126.com>
2026-04-30 13:24:41 +0800

    统一错误处理

---
+++ kiwi-bpmn/kiwi-bpmn-core/src/main/java/com/kiwi/bpmn/core/jobretry/JobRetryFailureSupport.java
@@ -0,0 +1,18 @@
+package com.kiwi.bpmn.core.jobretry;
+
+import org.camunda.bpm.engine.OptimisticLockingException;
+
+final class JobRetryFailureSupport {
+
+    private JobRetryFailureSupport() {
+    }
+
+    static boolean isOptimisticLockingOnChain(Throwable t) {
+        for (Throwable c = t; c != null; c = c.getCause()) {
+            if (c instanceof OptimisticLockingException) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
